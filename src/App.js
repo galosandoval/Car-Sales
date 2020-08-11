f276e39864ffc4fe4424dbf8cd7f2bf921daa2d7
Galo Sandoval <galosan92@gmail.com>
2020-08-11 13:54:10 -0700

    created a few components

--- src/App.js
+++ src/App.js
@@ -4,34 +4,27 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { initialState, appReducer } from './reducers/appReducer'
 
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+  console.log(initialState.additionalFeatures[0])
 
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  const addFeature = e => {
+    
+  }
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={initialState.car} />
+        <AddedFeatures car={initialState.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={initialState.additionalFeatures} />
+        <Total car={initialState.car} additionalPrice={initialState.additionalPrice} />
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { appReducer } from './reducers/appReducer';
 
+const store = createStore(appReducer)
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
